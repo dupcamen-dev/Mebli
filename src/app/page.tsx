991ebdb2b1c11378935a18f5a9781696afe19b17
dupcamen-dev <dupcamen-dev@github.com>
2026-07-20 17:20:04 +0300

    refactor: remove CTA and Philosophy sections from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import { Hero } from "@/components/sections/Hero";
 import { Process } from "@/components/sections/Process";
 import { Story } from "@/components/sections/Story";
-import { Philosophy } from "@/components/sections/Philosophy";
 import { Categories } from "@/components/sections/Categories";
 import { Reviews } from "@/components/sections/Reviews";
 import { Contact } from "@/components/sections/Contact";
-import { CTA } from "@/components/sections/CTA";
 
 export default function Home() {
   return (
@@ -13,10 +11,8 @@ export default function Home() {
       <Hero />
       <Process />
       <Story />
-      <Philosophy />
       <Categories />
       <Reviews />
-      <CTA />
       <Contact />
     </>
   );
